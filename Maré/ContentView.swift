afd11994ed8d6240fdb5076230a33fe2ae11609e
Vini R Jacob <vini_jacob@hotmail.com>
2026-09-23 18:34:25 -0400

    Initial Commit

---
+++ Maré/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maré
+//
+//  Created by Vinicius Ramos Jacob on 23/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maré/Mare_App.swift
@@ -0,0 +1,17 @@
+//
+//  Mare_App.swift
+//  Maré
+//
+//  Created by Vinicius Ramos Jacob on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Mare_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
