8d90cd6459c40437c0284eed5fdd5e461b8f5792
Vini R Jacob <vini_jacob@hotmail.com>
2026-09-23 20:15:39 -0400

    Updated Mare_App with .modelContainer for SwiftData

--- Maré/App/Mare_App.swift
+++ Maré/App/Mare_App.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Mare_App: App {
@@ -13,5 +14,6 @@ struct Mare_App: App {
         WindowGroup {
             RootView()
         }
+        .modelContainer(for: MoodRegister.self)
     }
 }
